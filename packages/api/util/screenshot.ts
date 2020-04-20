d880b9767330cdb5b4443fca430c0821566b48fc
Timo Staudinger <mail@timostaudinger.com>
2020-04-20 17:42:45 -0400

    Update puppeteer

--- packages/api/util/screenshot.ts
+++ packages/api/util/screenshot.ts
@@ -1,5 +1,5 @@
-import * as chrome from 'chrome-aws-lambda'
-import * as puppeteer from 'puppeteer-core'
+const chrome = require('chrome-aws-lambda')
+const puppeteer = require('puppeteer-core')
 
 const padding = 0
 
@@ -7,13 +7,13 @@ const getScreenshot = async (pageContent: string, targetId: string) => {
   const browser = await puppeteer.launch({
     args: chrome.args,
     executablePath: await chrome.executablePath,
-    headless: chrome.headless
+    headless: chrome.headless,
   })
 
   const page = await browser.newPage()
   await page.setContent(pageContent)
 
-  const rect = await page.evaluate(targetId => {
+  const rect = await page.evaluate((targetId: any) => {
     const element = document.getElementById(targetId)
     const {x, y, width, height} = element.getBoundingClientRect() as DOMRect
     return {left: x, top: y, width, height, id: element.id}
@@ -26,8 +26,8 @@ const getScreenshot = async (pageContent: string, targetId: string) => {
       x: rect.left - padding,
       y: rect.top - padding,
       width: rect.width + padding * 2,
-      height: rect.height + padding * 2
-    }
+      height: rect.height + padding * 2,
+    },
   })
 
   await browser.close()
