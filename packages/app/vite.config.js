db293c0a485c8e205b2b2e5f582328605ebb1eeb
Timo M. Staudinger <mail@timostaudinger.com>
2025-12-20 16:06:10 +0100

    Fix build

--- packages/app/vite.config.js
+++ packages/app/vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // If you used CRA proxy via setupProxy.js, put it here (example):
-    // proxy: { "/api": { target: "http://localhost:8080", changeOrigin: true } },
-  },
+  server: {},
 });
