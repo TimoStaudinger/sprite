1cb0d4ee36530e6a45b58f27989156a7d0e4d06d
Timo M. Staudinger <5621996+TimoStaudinger@users.noreply.github.com>
2026-03-29 22:02:53 -0400

    Remove Twitter link from header (#179)

--- src/header/Links.tsx
+++ src/header/Links.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faGithub, faTwitter} from '@fortawesome/free-brands-svg-icons'
+import {faGithub} from '@fortawesome/free-brands-svg-icons'
 import styles from './Links.module.css'
 
 const Links = () => (
@@ -12,14 +12,6 @@ const Links = () => (
       className={styles.item}
     >
       <FontAwesomeIcon icon={faGithub} />
-    </a>
-    <a
-      href="https://twitter.com/spritedotlink/"
-      title="Sprite on Twitter"
-      target="_new"
-      className={styles.item}
-    >
-      <FontAwesomeIcon icon={faTwitter} />
     </a>
   </div>
 )
