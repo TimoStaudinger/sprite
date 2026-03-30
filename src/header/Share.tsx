9ff27c68fd210c8ed9dc310fab0f896f22176ff3
Timo M. Staudinger <5621996+TimoStaudinger@users.noreply.github.com>
2026-03-29 22:16:39 -0400

    Add copy button next to embed URL in header (#180)
    * Add copy button next to embed URL in header
    
    * Fix copy button layout to be inline with input
    
    * Fix copy button to not resize on label change

--- src/header/Share.tsx
+++ src/header/Share.tsx
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react'
+import React, {useRef, useState} from 'react'
 import {encode} from '../util/code'
 import styles from './Share.module.css'
 
@@ -13,6 +13,16 @@ interface Props {
 
 const Share = ({code}: Props) => {
   const inputEl = useRef<HTMLInputElement>(null)
+  const [copied, setCopied] = useState(false)
+
+  const url = `${domain}/chart/${encode(code)}.png`
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(url).then(() => {
+      setCopied(true)
+      setTimeout(() => setCopied(false), 2000)
+    })
+  }
 
   return (
     <div className={styles.share}>
@@ -22,10 +32,13 @@ const Share = ({code}: Props) => {
       <input
         id="shareLink"
         readOnly
-        value={`${domain}/chart/${encode(code)}.png`}
+        value={url}
         ref={inputEl}
         onFocus={() => inputEl.current && inputEl.current.select()}
       />
+      <button className={styles.copyButton} onClick={handleCopy}>
+        {copied ? 'Copied!' : 'Copy'}
+      </button>
     </div>
   )
 }
